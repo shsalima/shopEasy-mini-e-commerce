fb962ad3764fba094d370ed02bdab8806dae6ebd
salima <sahisalima77@gmail.com>
2026-06-16 17:26:44 +0200

    travail sur contexte de cart

---
+++ context/CartContext.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { CartItem } from '@/app/types/CartItem';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+interface CartContextType {
+  cart: CartItem[];
+  addToCart: (item: Omit<CartItem, 'quantity'>) => void;
+  removeFromCart: (id: string) => void;
+  clearCart: () => void;
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+export function CartProvider({ children }: { children: React.ReactNode }) {
+  const [cart, setCart] = useState<CartItem[]>([]);
+
+  useEffect(() => {
+    const savedCart = localStorage.getItem('aurastore_cart');
+    if (savedCart) {
+      setCart(JSON.parse(savedCart));
+    }
+  }, []);
+
+  const saveCart = (newCart: CartItem[]) => {
+    setCart(newCart);
+    localStorage.setItem('aurastore_cart', JSON.stringify(newCart));
+  };
+
+  const addToCart = (item: Omit<CartItem, 'quantity'>) => {
+    const existing = cart.find((i) => i.id === item.id);
+    if (existing) {
+      saveCart(cart.map((i) => i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i));
+    } else {
+      saveCart([...cart, { ...item, quantity: 1 }]);
+    }
+  };
+
+  const removeFromCart = (id: string) => {
+    saveCart(cart.filter((item) => item.id !== id));
+  };
+
+  const clearCart = () => {
+    saveCart([]);
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  const context = useContext(CartContext);
+  if (!context) throw new Error('useCart must be used within a CartProvider');
+  return context;
+}
